8352f69211bc998b77508821f089b47aafb120a0
Hamza Rizwan <hamzarizwan243@gmail.com>
2022-11-30 01:53:01 +0530

    Cleanup

--- app/src/main/java/app/simple/inureunlocker/utils/ActivityUtils.kt
+++ app/src/main/java/app/simple/inureunlocker/utils/ActivityUtils.kt
@@ -3,139 +3,15 @@ package app.simple.inureunlocker.utils
 import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
-import android.content.pm.ComponentInfo
-import android.content.pm.PackageManager
-import android.os.Build
-import androidx.core.content.ContextCompat.startActivity
-import app.simple.inureunlocker.constants.IntentConstants
-import app.simple.inureunlocker.utils.NullSafety.isNotNull
 
 object ActivityUtils {
-    /**
-     * Launch a given activity class
-     */
-    @Throws
-    fun launchPackage(context: Context, packageName: String, packageId: String) {
-        val intent = Intent(Intent.ACTION_MAIN)
-        intent.addCategory(Intent.CATEGORY_LAUNCHER)
-        intent.component = ComponentName(packageName, packageId)
-        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_RESET_TASK_IF_NEEDED or Intent.FLAG_ACTIVITY_CLEAR_TOP
-        intent.putExtra(IntentConstants.ACTION_UNLOCK, IntentConstants.EXTRA_UNLOCK_DATA)
-        context.startActivity(intent)
-    }
-
     /**
      * Launch a given activity class with [Intent.setAction]
      */
     fun launchPackage(context: Context, packageName: String, packageId: String, action: String) {
-        val intent = Intent(IntentConstants.ACTION_UNLOCK)
+        val intent = Intent(action)
         intent.component = ComponentName(packageName, packageId)
         intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP
         context.startActivity(intent)
-    }
-
-    fun isEnabled(context: Context, packageName: String, clsName: String): Boolean {
-        val componentName = ComponentName(packageName, clsName)
-
-        return when (context.packageManager.getComponentEnabledSetting(componentName)) {
-            PackageManager.COMPONENT_ENABLED_STATE_DISABLED -> false
-            PackageManager.COMPONENT_ENABLED_STATE_ENABLED -> true
-            PackageManager.COMPONENT_ENABLED_STATE_DEFAULT ->       // We need to get the application info to get the component's default state
-                try {
-                    val packageInfo = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-                            context.packageManager
-                                .getPackageInfo(
-                                    packageName,
-                                    PackageManager.PackageInfoFlags.of(
-                                        (PackageManager.GET_ACTIVITIES or PackageManager.MATCH_DISABLED_COMPONENTS).toLong()
-                                    )
-                                )
-                        } else {
-                            @Suppress("DEPRECATION")
-                            context.packageManager
-                                .getPackageInfo(
-                                    packageName,
-                                    PackageManager.GET_ACTIVITIES or PackageManager.MATCH_DISABLED_COMPONENTS
-                                )
-                        }
-                    } else {
-                        @Suppress("deprecation")
-                        context.packageManager.getPackageInfo(
-                            packageName, PackageManager.GET_ACTIVITIES
-                                    or PackageManager.GET_DISABLED_COMPONENTS
-                        )
-                    }
-
-                    val components: ArrayList<ComponentInfo> = ArrayList()
-
-                    if (packageInfo.activities.isNotNull()) {
-                        for (i in packageInfo.activities) {
-                            components.add(i)
-                        }
-                    }
-
-                    for (componentInfo in components) {
-                        if (componentInfo.name == clsName) {
-                            return componentInfo.isEnabled
-                        }
-                    }
-
-                    // the component is not declared in the AndroidManifest
-                    false
-                } catch (e: PackageManager.NameNotFoundException) {
-                    // the package isn't installed on the device
-                    false
-                }
-
-            else -> {
-                try {
-                    val packageInfo = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-                            context.packageManager
-                                .getPackageInfo(
-                                    packageName,
-                                    PackageManager.PackageInfoFlags.of(
-                                        (PackageManager.GET_ACTIVITIES or PackageManager.MATCH_DISABLED_COMPONENTS).toLong()
-                                    )
-                                )
-                        } else {
-                            @Suppress("DEPRECATION")
-                            context.packageManager
-                                .getPackageInfo(
-                                    packageName,
-                                    PackageManager.GET_ACTIVITIES or PackageManager.MATCH_DISABLED_COMPONENTS
-                                )
-                        }
-                    } else {
-                        @Suppress("deprecation")
-                        context.packageManager.getPackageInfo(
-                            packageName, PackageManager.GET_ACTIVITIES
-                                    or PackageManager.GET_DISABLED_COMPONENTS
-                        )
-                    }
-
-                    val components: ArrayList<ComponentInfo> = ArrayList()
-
-                    if (packageInfo.activities.isNotNull()) {
-                        for (i in packageInfo.activities) {
-                            components.add(i)
-                        }
-                    }
-
-                    for (componentInfo in components) {
-                        if (componentInfo.name == clsName) {
-                            return componentInfo.isEnabled
-                        }
-                    }
-
-                    // the component is not declared in the AndroidManifest
-                    false
-                } catch (e: PackageManager.NameNotFoundException) {
-                    // the package isn't installed on the device
-                    false
-                }
-            }
-        }
     }
 }
--- app/src/main/java/app/simple/inureunlocker/utils/MarketUtils.kt
+++ app/src/main/java/app/simple/inureunlocker/utils/MarketUtils.kt
@@ -24,12 +24,16 @@ object MarketUtils {
         /**
          * find all applications able to handle our rateIntent
          */
-        val otherApps: List<ResolveInfo> = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-            context.packageManager.queryIntentActivities(rateIntent, PackageManager.ResolveInfoFlags.of(0))
-        } else {
-            @Suppress("DEPRECATION")
-            context.packageManager.queryIntentActivities(rateIntent, 0)
-        }
+        val otherApps: List<ResolveInfo> =
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                context.packageManager.queryIntentActivities(
+                    rateIntent,
+                    PackageManager.ResolveInfoFlags.of(0)
+                )
+            } else {
+                @Suppress("DEPRECATION")
+                context.packageManager.queryIntentActivities(rateIntent, 0)
+            }
 
         for (otherApp in otherApps) {
             /**
@@ -37,7 +41,10 @@ object MarketUtils {
              */
             if (otherApp.activityInfo.applicationInfo.packageName == "com.android.vending") {
                 val otherAppActivity = otherApp.activityInfo
-                val componentName = ComponentName(otherAppActivity.applicationInfo.packageName, otherAppActivity.name)
+                val componentName = ComponentName(
+                    otherAppActivity.applicationInfo.packageName,
+                    otherAppActivity.name
+                )
 
                 /**
                  * make sure it does NOT open in the stack of your activity
@@ -71,85 +78,11 @@ object MarketUtils {
          * if GP not present on device, open web browser
          */
         if (!marketFound) {
-            val webIntent = Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=$packageName"))
+            val webIntent = Intent(
+                Intent.ACTION_VIEW,
+                Uri.parse("https://play.google.com/store/apps/details?id=$packageName")
+            )
             context.startActivity(webIntent)
         }
-    }
-
-    /**
-     * Opens app's page on Amazon App Store
-     *
-     * @param context Context of the environment
-     * @param packageName Package ID of the app
-     */
-    fun openAppOnAmazonStore(context: Context, packageName: String) {
-
-        val rateIntent = Intent(Intent.ACTION_VIEW, Uri.parse("amzn://apps/android?p=$packageName"))
-        var marketFound = false
-
-        /**
-         * find all applications able to handle our rateIntent
-         */
-        val otherApps: List<ResolveInfo> = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-            context.packageManager.queryIntentActivities(rateIntent, PackageManager.ResolveInfoFlags.of(0))
-        } else {
-            @Suppress("DEPRECATION")
-            context.packageManager.queryIntentActivities(rateIntent, 0)
-        }
-
-        for (otherApp in otherApps) {
-            /**
-             * look for Google Play application
-             */
-            if (otherApp.activityInfo.applicationInfo.packageName == "com.amazon.venezia") {
-                val otherAppActivity = otherApp.activityInfo
-                val componentName = ComponentName(otherAppActivity.applicationInfo.packageName, otherAppActivity.name)
-
-                /**
-                 * make sure it does NOT open in the stack of your activity
-                 */
-                rateIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-
-                /**
-                 * task re -parenting if needed
-                 */
-                rateIntent.addFlags(Intent.FLAG_ACTIVITY_RESET_TASK_IF_NEEDED)
-
-                /**
-                 * if the Google Play was already open in a search result
-                 * this make sure it still go to the app page you requested
-                 */
-                rateIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
-
-                /**
-                 * this make sure only the Google Play app is allowed to
-                 * intercept the intent
-                 */
-                rateIntent.component = componentName
-                context.startActivity(rateIntent)
-                marketFound = true
-
-                break
-            }
-        }
-
-        /**
-         * if GP not present on device, open web browser
-         */
-        if (!marketFound) {
-            val webIntent = Intent(Intent.ACTION_VIEW, Uri.parse("http://www.amazon.com/gp/mas/dl/android?p=$packageName"))
-            context.startActivity(webIntent)
-        }
-    }
-
-    /**
-     * Opens app's page on FDroid
-     *
-     * @param context Context of the environment
-     * @param packageName Package ID of the app
-     */
-    fun openAppOnFdroid(context: Context, packageName: String) {
-        val webIntent = Intent(Intent.ACTION_VIEW, Uri.parse("https://www.f-droid.org/en/packages/$packageName\\/"))
-        context.startActivity(webIntent)
     }
 }
